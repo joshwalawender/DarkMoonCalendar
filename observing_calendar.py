dd4da0cabe357ab8f5fedac6a5370ab39d02125b
Josh Walawender <jmwalawender@gmail.com>
2016-06-25 23:55:45 -1000

    Output CSV file for importing in to a Google Calendar

--- observing_calendar.py
+++ observing_calendar.py
@@ -7,7 +7,8 @@ import sys
 import os
 import argparse
 
-# from datetime import datetime as dt
+from datetime import datetime as dt
+from datetime import timedelta as tdelta
 import pytz
 
 import numpy as np
@@ -20,13 +21,29 @@ from astroplan import FixedTarget, Observer, moon
 import ephem
 
 ##-------------------------------------------------------------------------
-## Schedule Night
+## Print Night
 ##-------------------------------------------------------------------------
-def schedule_night(sunset, dusk, string='', tz='UTC'):
-#     print('{} {} {}'.format(sunset.iso, dusk.iso.split()[1], string))
-    sunset_string = sunset.strftime('%Y-%m-%d %H:%M:%S')
-    dusk_string = dusk.strftime('%H:%M:%S')
-    print('{}, {}, {}'.format(sunset_string, dusk_string, string))
+def schedule_night(FO, local_sunset, local_dusk, title):
+    twohours = tdelta(seconds=60.*60.*2.)
+    descr = 'Sunset: {} | Twilight End: {}'.format(
+            local_sunset.strftime('%I:%M %p'),
+            local_dusk.strftime('%I:%M %p'),
+            )
+    line = '{}, {}, {}, {}, {}, {}\n'.format(
+            title,
+            (local_sunset-twohours).strftime('%m/%d/%Y'),
+            (local_sunset-twohours).strftime('%I:%M %p'),
+            (local_sunset-twohours).strftime('%m/%d/%Y'),
+            '11:59 PM',
+            descr)
+    print('{}, {}, {}'.format(
+          local_sunset.strftime('%Y-%m-%d %H:%M:%S'),
+          local_dusk.strftime('%H:%M:%S'), title))
+    FO.write(line)
+
+def print_night(sunset, dusk, string=''):
+    print('{}, {}, {}'.format(sunset_string.strftime('%Y-%m-%d %H:%M:%S'),\
+                              dusk_string.strftime('%H:%M:%S'), string))
 
 
 ##-------------------------------------------------------------------------
@@ -44,7 +61,7 @@ def main():
     ## add arguments
     parser.add_argument("-y", "--year",
         type=int, dest="year",
-        default=2016,
+        default=-1,
         help="The calendar year to analyze")
     parser.add_argument("-s", "--site",
         type=str, dest="site",
@@ -56,7 +73,7 @@ def main():
         help='pytz timezone name')
     parser.add_argument("-d", "--dark_time",
         type=float, dest="dark_time",
-        default=3,
+        default=2,
         help='Minimum dark time required (hours)')
     parser.add_argument("-w", "--wait_time",
         type=float, dest="wait_time",
@@ -64,6 +81,9 @@ def main():
         help='Maximum time after dusk to wait for moon set (hours)')
 
     args = parser.parse_args()
+
+    if args.year == -1:
+        args.year = dt.now().year
 
     ##-------------------------------------------------------------------------
     ## 
@@ -84,41 +104,52 @@ def main():
     start_date = Time(date_iso_string, format='isot', scale='utc', location=loc)
     sunset = obs.sun_set_time(start_date, which='nearest')
 
-    while int(np.floor(sunset.decimalyear)) == args.year:
-        sunset = obs.sun_set_time(sunset+oneday, which='nearest')
-        local_sunset = sunset.to_datetime(localtz)
+    filename = 'DarkMoonCalendar_{:4d}.csv'.format(args.year)
+    if os.path.exists(filename): os.remove(filename)
+    with open(filename, 'w') as FO:
+        hdr = 'Subject, Start date, Start time, End date, End time, Description'
+        FO.write(hdr+'\n')
 
-        dusk = obs.twilight_evening_astronomical(sunset, which='next')
-        local_dusk = dusk.to_datetime(localtz)
+        while sunset < start_date + 365*oneday:
+            sunset = obs.sun_set_time(sunset+oneday, which='nearest')
+            local_sunset = sunset.to_datetime(localtz)
 
-        m = moon.get_moon(sunset, loc)
-        illum = moon.moon_illumination(sunset, loc)
+            dusk = obs.twilight_evening_astronomical(sunset, which='next')
+            local_dusk = dusk.to_datetime(localtz)
 
-        pyephem_site.date = sunset.to_datetime().strftime('%Y/%m/%d %H:%M')
-        pyephem_moon.compute(pyephem_site)
+            m = moon.get_moon(sunset, loc)
+            illum = moon.moon_illumination(sunset, loc)
 
-        if m.alt < 0*u.degree:
-#             est_moon_rise = obs.target_rise_time(sunset, m, which='next')
-#             m2 = moon.get_moon(est_moon_rise, loc)
-#             moon_rise = obs.target_rise_time(est_moon_rise, m2, which='nearest')
-            pyephem_moon_rise = Time(pyephem_site.next_rising(ephem.Moon()).datetime())
-            moon_rise = pyephem_moon_rise
-            local_moon_rise = moon_rise.to_datetime(localtz)
-            time_to_rise = moon_rise - dusk
-            if time_to_rise.sec*u.second > args.dark_time*u.hour:
-                schedule_night(local_sunset, local_dusk, 'dark until {:.0f}% moon rise at {}'.format(illum*100., local_moon_rise.strftime('%H:%M:%S')))
-        else:
-#             est_moon_set = obs.target_set_time(sunset, m, which='next')
-#             m2 = moon.get_moon(est_moon_set, loc)
-#             moon_set = obs.target_set_time(sunset, m2, which='nearest')
-            pyephem_moon_set = Time(pyephem_site.next_setting(ephem.Moon()).datetime())
-            moon_set = pyephem_moon_set
-            local_moon_set = moon_set.to_datetime(localtz)
-            time_to_set = moon_set - dusk
-            if moon_set < dusk:
-                schedule_night(local_sunset, local_dusk, 'dark ({:.0f}%)'.format(illum*100.))
-            elif time_to_set.sec*u.second < args.wait_time*u.hour:
-                schedule_night(local_sunset, local_dusk, 'dark after {:.0f}% moon sets at {}'.format(illum*100., local_moon_set.strftime('%H:%M:%S')))
+            pyephem_site.date = sunset.to_datetime().strftime('%Y/%m/%d %H:%M')
+            pyephem_moon.compute(pyephem_site)
+
+            if m.alt < 0*u.degree:
+    #             est_moon_rise = obs.target_rise_time(sunset, m, which='next')
+    #             m2 = moon.get_moon(est_moon_rise, loc)
+    #             moon_rise = obs.target_rise_time(est_moon_rise, m2, which='nearest')
+                pyephem_moon_rise = Time(pyephem_site.next_rising(ephem.Moon()).datetime())
+                moon_rise = pyephem_moon_rise
+                local_moon_rise = moon_rise.to_datetime(localtz)
+                time_to_rise = moon_rise - dusk
+                if time_to_rise.sec*u.second > args.dark_time*u.hour:
+                    title = 'Dark until {} when {:.0f}% moon rises'.format(
+                            local_moon_rise.strftime('%I:%M %p'), illum*100.)
+                    schedule_night(FO, local_sunset, local_dusk, title)
+            else:
+    #             est_moon_set = obs.target_set_time(sunset, m, which='next')
+    #             m2 = moon.get_moon(est_moon_set, loc)
+    #             moon_set = obs.target_set_time(sunset, m2, which='nearest')
+                pyephem_moon_set = Time(pyephem_site.next_setting(ephem.Moon()).datetime())
+                moon_set = pyephem_moon_set
+                local_moon_set = moon_set.to_datetime(localtz)
+                time_to_set = moon_set - dusk
+                if moon_set < dusk:
+                    title = 'Dark ({:.0f}% moon)'.format(illum*100.)
+                    schedule_night(FO, local_sunset, local_dusk, title)
+                elif time_to_set.sec*u.second < args.wait_time*u.hour:
+                    title = 'Dark after {} when {:.0f}% moon sets'.format(
+                            local_moon_set.strftime('%I:%M %p'), illum*100.)
+                    schedule_night(FO, local_sunset, local_dusk, title)
 
 if __name__ == '__main__':
     main()
